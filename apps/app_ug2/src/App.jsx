e3e05baae8d46baec348f54256f372b0f91ddd34
OndrejHanak666 <oh2004@seznam.cz>
2025-03-21 12:33:27 +0100

    a

--- apps/app_ug2/src/App.jsx
+++ apps/app_ug2/src/App.jsx
@@ -1,7 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { AppCanvas } from '@hrbolek/uoisfrontend-gql-shared'
-import { AppRouter } from './AppRouter';
+/*import { AppRouter } from './AppRouter';*/
+
+const User = ({ name, surname, children }) => {
+    return (
+        <div>{name} {surname}, {children} </div>
+    );
+};
+
+const Cigos = ({pocet}) => {
+    return (
+        <div><b>Cigan: {pocet}</b></div>
+    )
+}
+
+
+
+
+
 
 export const App = () => {
     return (
@@ -16,8 +33,9 @@ export const App = () => {
                     </Navbar.Collapse>
                 </Container>
             </Navbar> */}
-            
-            <AppRouter />
+           Hello World  <User name = "John" surname = "Newbie" children = "3"></User> <Cigos pocet = "5"></Cigos>
+           <img src="chrudimak.png" alt="Český výsadkář z Chrudimi s červeným baretem" />
+           {/*<AppRouter />*/} 
         </AppCanvas>    
         // {/* </Container> */}
     )
