62299a4a49ca241ed4830875b94ee4068de7c402
OndrejHanak666 <oh2004@seznam.cz>
2025-04-01 08:09:15 +0200

    abc

--- apps/app_moje/src/App.jsx
+++ apps/app_moje/src/App.jsx
@@ -3,6 +3,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { AppCanvas, createAsyncGraphQLAction, useAsyncAction } from '@hrbolek/uoisfrontend-gql-shared'
 /*import { AppRouter } from './AppRouter';*/
 
+const User = ({ name, surname, children }) => {
+    return (
+        <div>{name} {surname}, {children} </div>
+    );
+};
+
+
+
+/*const fetchUserAction = createAsyncGraphQLAction('{
+    userPage {
+        id
+        name
+        surname
+    }
+
+}')*/
+
+const FirstEntity = () => {
+    const { loading, error, entity, Dispa } = useAsyncAction(fetchUserAction, { id: "123" }, { deferred: true});
+
+    const data = read();
+    return <div>User: {data}</div>
+}
 
 
 
@@ -20,6 +43,8 @@ export const App = () => {
                     </Navbar.Collapse>
                 </Container>
             </Navbar> */}
+           Hello World  <User name = "John" surname = "Newbie" children = "3"></User> 
+           <img src="chrudimak.png" alt="Český výsadkář z Chrudimi s červeným baretem" />
            {/*<AppRouter />*/} 
         </AppCanvas>    
         // {/* </Container> */}
